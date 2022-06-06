3c27851964b3b845a2ed25eee9b90bf9ba2abc21
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-06-06 19:02:26 -0400

    started pseudo code for javascript

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,2 +1,14 @@
+// step one: put current date using moment 
+// step two: compare the current hour w/ time-blokcs
+// step three: translate time-blocks to military time 
+// step four: use css classes for present, past and future
+// compare the time blocks with moment()
+
+// if momemt () > time blocks => past
+// if moment () == time block => past
+// if moment () < time-blocks => future
+
+// step four: grabthe txt area value and store it in local storage 
+
 
 
