d173084773a8bde81a715306a4e7570fb375321f
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-06-12 01:38:15 -0400

    reconstruction on display time color blocks function

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,7 +1,7 @@
-// step one: put current date using moment 
+// step one: put current date using moment
 // step two: compare the current hour w/ time-blokcs
-// step three: translate time-blocks to military time 
-// step four: use css classes for present, past and future 
+// step three: translate time-blocks to military time
+// step four: use css classes for present, past and future
 
 // compare the time blocks with moment()
 
@@ -9,44 +9,36 @@
 // if moment () == time block => past
 // if moment () < time-blocks => future
 
-// step four: grab the txt area value and store it in local storage 
+// step four: grab the txt area value and store it in local storage
 
-
-var timeBlocksArr=[9,10,11,12,13,14,15,16,17] // translate this time-blocks to miltary time 
+var timeBlocksArr = [9, 10, 11, 12, 13, 14, 15, 16, 17]; // translate this time-blocks to miltary time
 // var textAreaEl = $("textarea")
 
-var currenthour= moment().hour()
-var currentDay= $("#currentDay") // kind of like document.quereySelector(#currentDay)
-console.log(currenthour)
+var currenthour = moment().hour();
+var currentDay = $("#currentDay"); // kind of like document.quereySelector(#currentDay)
+console.log(currenthour);
 
-var systemTime=moment().format("dddd, MMMM Do")
-currentDay.text(systemTime) // kind of like currentDay.textContent=systemTime
+var systemTime = moment().format("dddd, MMMM Do");
+currentDay.text(systemTime); // kind of like currentDay.textContent=systemTime
 
-
-function displayTimeBlockColors(){
-    for (var i = 0; i < timeBlocksArr.length; i++) {
-        
-        
-    }
+function displayTimeBlockColors() {
+  for (var i = 0; i < timeBlocksArr.length; i++) {
+    // this if would be past
+    var currentTextEl = $("#" + timeBlocksArr[i]);
+    if(currenthour>timeBlocksArr[i]){
+           currentTextEl.addClass("past")
+      } 
+      // present 
+      else if(currenthour=== timeBlocksArr[i]){
+         currentTextEl.addClass("present")
+      }
+      // future
+      else {
+         currentTextEl.addClass("future") 
+      }
+ }
 }
 
-// function displayTimeBlockColors(){
-//     for(var i=0; i < textAreaEl.length;i++){
-//         //this would be past 
-//         var currentTextEl = $("#",textAreaEl[i])
+displayTimeBlockColors()
 
-//         if(currenthour>textAreaEl[i]){
-          
-//            currentTextEl.addClass("past")[i]
-//          }
-//          else if(currenthour== textAreaEl[i]){
-//             currentTextEl.addClass("present")
-//          }
-//          else if(currenthour < textAreaEl[i]){
-//             currentTextEl.addClass("future") 
-//          }
-//     }
-// }
-
-// displayTimeBlockColors()
 
